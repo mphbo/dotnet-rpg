b2353f778945865c4967f719f910401c0d9b53bb
logan.thomas <logan.thomas@forsta.com>
2022-08-04 20:24:10 -0700

    added a couple more routes and now am starting the work on the character service

--- Controllers/CharacterController.cs
+++ Controllers/CharacterController.cs
@@ -10,12 +10,31 @@ namespace dotnet_rpg.Controllers
     [Route("api/[controller]")]
     public class CharacterController : ControllerBase
     {
-        private static Character knight = new Character();
+        private static List<Character> characters = new List<Character>
+        {
+            new Character(),
+            new Character { Name = "Sam", Id = 1 },
+            new Character { Name = "Marry", Id = 2 },
+        };
 
         [HttpGet]
-        public ActionResult<Character> Get()
+        [Route("GetAll")]
+        public ActionResult<List<Character>> Get()
         {
-            return Ok(knight);
+            return Ok(characters);
+        }
+
+        [HttpGet("{id}")]
+        public ActionResult<Character> GetSingle(int id)
+        {
+            return Ok(characters.FirstOrDefault(c => c.Id == id));
+        }
+
+        [HttpPost]
+        public ActionResult<List<Character>> AddCharacter(Character newCharacter)
+        {
+            characters.Add(newCharacter);
+            return Ok(characters);
         }
     }
 }
--- Models/RpgClass.cs
+++ Models/RpgClass.cs
@@ -2,9 +2,11 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using System.Text.Json.Serialization;
 
 namespace dotnet_rpg.Models
 {
+    [JsonConverter(typeof(JsonStringEnumConverter))]
     public enum RpgClass
     {
         Knight = 1,
