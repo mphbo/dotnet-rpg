ad088135c0b601e4626789f28f6470d0d762344d
logan.thomas <logan.thomas@forsta.com>
2022-08-05 19:41:38 -0700

    added some asynchronous calls

--- Services/CharacterService/CharacterService.cs
+++ Services/CharacterService/CharacterService.cs
@@ -15,18 +15,18 @@ namespace dotnet_rpg.Services.CharacterService
             new Character { Name = "Marry", Id = 2 },
         };
 
-        public List<Character> AddCharacter(Character newCharacter)
+        public async Task<List<Character>> AddCharacter(Character newCharacter)
         {
             characters.Add(newCharacter);
             return characters;
         }
 
-        public List<Character> GetAllCharacters()
+        public async Task<List<Character>> GetAllCharacters()
         {
             return characters;
         }
 
-        public Character GetCharacterById(int id)
+        public async Task<Character> GetCharacterById(int id)
         {
             return characters.FirstOrDefault(c => c.Id == id);
         }
